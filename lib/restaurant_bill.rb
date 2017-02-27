0d225a178a95e83dbd3f8305ba4eafa58182fc73
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-02-27 08:08:40 -0800

    get started

--- lib/restaurant_bill.rb
+++ lib/restaurant_bill.rb
@@ -1,12 +1,27 @@
-# class RestaurantBill
-#   attr_reader :ordered_items
+class RestaurantBill
+  attr_reader :ordered_items
 
-#   def initialize
-#     @ordered_items = []
-#   end
+  def initialize
+    @ordered_items = []
+  end
 
-#   def order_item(item, cost)
-#     @ordered_items << [item, cost]
-#   end
+  def order_item(item, cost)
+    @ordered_items << [item, cost]
+  end
 
-# end
+  def item_total
+    total =  0
+    @ordered_items.each do |item|
+      total += item[1]
+    end
+
+    total_with_tax = add_tax(total)
+  end
+
+  def add_tax(price)
+    tax = price * 0.057
+    total = price + tax
+    (total * 100).round / 100.0
+  end
+
+end
--- specs/restaurant_bill_spec.rb
+++ specs/restaurant_bill_spec.rb
@@ -2,3 +2,35 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/restaurant_bill'
+
+
+describe 'Test restaurant_bill' do
+
+  it "Can create an instance of RestaurantBill" do
+    bill = RestaurantBill.new
+
+    bill.class.must_equal RestaurantBill
+  end
+
+  it "Bill initializes with no ordered items" do
+    bill = RestaurantBill.new
+
+    bill.ordered_items.must_be_empty
+  end
+
+  it "Bill has one item, after ordering" do
+    bill = RestaurantBill.new
+    bill.order_item("Waffles", 5)
+
+    bill.ordered_items.length.must_equal 1
+  end
+
+  it "Bill has total cost of items, with tax" do
+    bill = RestaurantBill.new
+    bill.order_item("Waffles", 5)
+    bill.order_item("Juice", 2)
+    bill.order_item("Fruit", 4)
+
+    bill.item_total.must_equal 11.63
+  end
+end
